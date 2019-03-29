77509b10dfcc7a398d5d886906ecb5dc60b9a27e
Ekaterina Krivich <ekaterinak@heathmont.net>
2019-03-29 16:50:15 +0200

    add mqtt

---
+++ temp-humid-mongoose/build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20190329-144023";
+const char *build_timestamp = "2019-03-29T14:40:23Z";
+const char *build_version = "1.0";
---
+++ temp-humid-mongoose/build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20190329-144022/2.12.1-gdd403d6";
+const char *mg_build_timestamp = "2019-03-29T14:40:22Z";
+const char *mg_build_version = "2.12.1";
