a685a3f919f9781e59a1343c029399bf886b2e3a
Ekaterina Krivich <ekaterinak@heathmont.net>
2019-07-04 17:09:59 +0300

    Update raspberry_script.py

--- raspberry-arduino_dht11/raspberry_script.py
+++ raspberry-arduino_dht11/raspberry_script.py
@@ -1,6 +1,28 @@
 import serial
 ser = serial.Serial('/dev/ttyACM0', 9600)
-while 1: 
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8080
+class myHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+    #Handler for the GET requests
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        # Send the html message
+        self.wfile.write("Hello World !")
+        return
+
+Handler = myHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+print("serving at port", PORT)
+httpd.serve_forever()
+
+while 1:
     if(ser.in_waiting > 0):
         line = ser.readline()
-        print(line)
+        temp, humid = line.split("|")
+        print("t = ", temp, "\n")
+        print("h = ", humid, "\n")
