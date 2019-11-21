170b53366305edb81cdb71aa8f02bc41b0da6bfd
Ekaterina Krivich <ekaterinak@heathmont.net>
2019-11-21 09:51:42 +0200

    Create dht11.c

---
+++ raspberry-arduino_dht11/dht11.c
@@ -0,0 +1,37 @@
+#include "DHT.h"
+
+#define DHTPIN 7     // Digital pin connected to the DHT sensor
+
+#define DHTTYPE DHT11   // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("DHTxx test!"));
+
+  dht.begin();
+}
+
+void loop() {
+  delay(2000);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.print(F("°C "));
+  Serial.print(F(" Heat index: "));
+  Serial.print(hic);
+  Serial.println(F("°C "));
+}
